b5166d191419ecfe65df2585b4d838aeb0e66db2
Gozdehan <gozdehanozturk@gmail.com>
2015-09-04 14:12:07 +0300

    set default border kısmı tamamlandı

--- createboard.js
+++ createboard.js
@@ -26,3 +26,16 @@ $(".cBset-footer button").click(function(){
   });
 }
 });
+
+ $(".open-close").click(function(){
+      if("none" == $(".submenu").css("display")){
+        $(".submenu").css({
+          display:"block"
+        });
+     } 
+     else if("block" == $(".submenu").css("display")){
+        $(".submenu").css({
+          display:"none"
+        });
+      }
+   });
