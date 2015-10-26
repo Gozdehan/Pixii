f20e0f5f19c88d49bd2cfc8ffd0bc09df6363c59
Gozdehan <gozdehanozturk@gmail.com>
2015-10-27 00:41:32 +0200

    personal kısmındaki hata düzeltildi ve ekleme yapıldı

--- personal.js
+++ personal.js
@@ -1,5 +1,40 @@
 $(document).ready(function(){
-$(".submenu li:first-child a").click(function(){
-	$(".sidebar-list li:first-child a").addClass("selected-sidebarList");
-});
+  $('.sidebar-list li a[data-sidebar="avatar"]').addClass('selected-sidebarList');
+
+  $(".sidebar-list li a").click(function(){
+	var type = $(this).attr("data-sidebar");
+	console.log(type);
+    
+  if(type = "avatar"){
+    $(".sidebar-list li a").removeClass("selected-sidebarList");
+        $(this).addClass("selected-sidebarList");
+  }
+    else if(type = "date"){
+      $(".sidebar-list li a").removeClass("selected-sidebarList");
+        $(this).addClass("selected-sidebarList");
+
+        $(".userData-nameAvatar").css({
+        	display:"none"
+        });
+        $("userData-dateTime").css({
+        	display:"block"
+        });
+   }
+   else if(type = "email"){
+      $(".sidebar-list li a").removeClass("selected-sidebarList");
+        $(this).addClass("selected-sidebarList");
+   }
+   else if(type = "game"){
+     $(".sidebar-list li a").removeClass("selected-sidebarList");
+        $(this).addClass("selected-sidebarList");
+   }
+   else if(type = "change"){
+       $(".sidebar-list li a").removeClass("selected-sidebarList");
+        $(this).addClass("selected-sidebarList");
+   }
+   else if(type = "remove"){
+       $(".sidebar-list li a").removeClass("selected-sidebarList");
+        $(this).addClass("selected-sidebarList");
+   }
+  });
 });
