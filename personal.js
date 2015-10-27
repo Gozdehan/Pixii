e6da8ac69b1d4573877aede65c811e251c671f41
Gozdehan <gozdehanozturk@gmail.com>
2015-10-27 23:32:10 +0200

    personal settings sidebar sayfaları tamamlandı

--- personal.js
+++ personal.js
@@ -5,36 +5,162 @@ $(document).ready(function(){
 	var type = $(this).attr("data-sidebar");
 	console.log(type);
     
-  if(type = "avatar"){
+  if(type == "avatar"){
     $(".sidebar-list li a").removeClass("selected-sidebarList");
         $(this).addClass("selected-sidebarList");
+
+        $(".userData-nameAvatar").css({
+          display:"block"
+        });
+        $(".userData-dateTime").css({
+          display:"none"
+        });
+        $(".userData-password").css({
+          display:"none"
+        });
+        $(".userData-email").css({
+          display:"none"
+        });
+         $(".userData-pGame").css({
+          display:"none"
+        });
+         $(".userData-remove").css({
+          display:"none"
+        });
   }
-    else if(type = "date"){
+    else if(type == "date"){
       $(".sidebar-list li a").removeClass("selected-sidebarList");
         $(this).addClass("selected-sidebarList");
 
         $(".userData-nameAvatar").css({
         	display:"none"
         });
-        $("userData-dateTime").css({
+        $(".userData-dateTime").css({
         	display:"block"
         });
+        $(".userData-password").css({
+          display:"none"
+        });
+        $(".userData-email").css({
+          display:"none"
+        });
+        $(".userData-pGame").css({
+          display:"none"
+        });
+        $(".userData-remove").css({
+          display:"none"
+        });
    }
-   else if(type = "email"){
+   else if(type == "email"){
       $(".sidebar-list li a").removeClass("selected-sidebarList");
         $(this).addClass("selected-sidebarList");
+
+         $(".userData-nameAvatar").css({
+          display:"none"
+        });
+        $(".userData-dateTime").css({
+          display:"none"
+        });
+        $(".userData-password").css({
+          display:"none"
+        });
+        $(".userData-email").css({
+          display:"block"
+        });
+        $(".userData-pGame").css({
+          display:"none"
+        });
+        $(".userData-remove").css({
+          display:"none"
+        });
    }
-   else if(type = "game"){
+   else if(type == "game"){
      $(".sidebar-list li a").removeClass("selected-sidebarList");
         $(this).addClass("selected-sidebarList");
+        
+        $(".userData-pGame").css({
+          display:"block"
+        });
+        $(".userData-nameAvatar").css({
+          display:"none"
+        });
+        $(".userData-dateTime").css({
+          display:"none"
+        });
+        $(".userData-password").css({
+          display:"none"
+        });
+        $(".userData-email").css({
+          display:"none"
+        });
+        $(".userData-remove").css({
+          display:"none"
+        });
    }
-   else if(type = "change"){
+   else if(type == "change"){
        $(".sidebar-list li a").removeClass("selected-sidebarList");
         $(this).addClass("selected-sidebarList");
+
+        $(".userData-nameAvatar").css({
+          display:"none"
+        });
+        $(".userData-dateTime").css({
+          display:"none"
+        });
+        $(".userData-password").css({
+          display:"block"
+        });
+        $(".userData-email").css({
+          display:"none"
+        });
+        $(".userData-pGame").css({
+          display:"none"
+        });
+        $(".userData-remove").css({
+          display:"none"
+        });
    }
-   else if(type = "remove"){
+   else if(type == "remove"){
        $(".sidebar-list li a").removeClass("selected-sidebarList");
         $(this).addClass("selected-sidebarList");
+        
+        $(".userData-remove").css({
+          display:"block"
+        });
+        $(".userData-nameAvatar").css({
+          display:"none"
+        });
+        $(".userData-dateTime").css({
+          display:"none"
+        });
+        $(".userData-password").css({
+          display:"none"
+        });
+        $(".userData-email").css({
+          display:"none"
+        });
+        $(".userData-pGame").css({
+          display:"none"
+        });
    }
   });
 });
+ 
+ $(document).ready(function(){
+   $(".open-close").click(function(){
+      if("none" == $(".submenu").css("display")){        
+        $(".submenu").css({
+          display:"block"
+        });
+
+        setTimeout(function () {
+          menuDurumAcik = true;
+        }, 10);  
+     } 
+     else if("block" == $(".submenu").css("display")){
+        menuKapat();
+      }
+   });
+
+  $("body").click(menuKapat);
+ });
